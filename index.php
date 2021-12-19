d2cb5504310c3f7f53ce7659b5c14451703b1295
sayan <sayansaha742@gmail.com>
2021-12-19 19:10:58 +0000

    add

--- index.php
+++ index.php
@@ -1,261 +1,521 @@
 <?php 
+
     include 'functions.php'; 
+
     getHeader();
+
 ?>
+
 <div id="carouselExampleControls" class="carousel slide bs-slider box-slider" data-ride="carousel" data-pause="hover" data-interval="false" >
+
 		<!-- Indicators -->
+
 		<ol class="carousel-indicators">
+
 			<li data-target="#carouselExampleControls" data-slide-to="0" class="active"></li>
+
 			<li data-target="#carouselExampleControls" data-slide-to="1"></li>
+
 			<li data-target="#carouselExampleControls" data-slide-to="2"></li>
+
 		</ol>
+
 		<div class="carousel-inner" role="listbox">
+
 			<div class="carousel-item active">
+
 				<div id="home" class="first-section" style="background-image:url('images/slider-01.jpg');">
+
 					<div class="dtab">
+
 						<div class="container">
+
 							<div class="row">
+
 								<div class="col-md-12 col-sm-12 text-right">
+
 									<div class="big-tagline">
-										<h2><strong>RestinPhilipDBS </strong> education College</h2>
+
+										<h2 class="text-center"><strong>Vincent</strong> High School</h2>
+
 									</div>
+
 								</div>
+
 							</div><!-- end row -->            
+
 						</div><!-- end container -->
+
 					</div>
+
 				</div><!-- end section -->
+
 			</div>
+
 			<div class="carousel-item">
+
 				<div id="home" class="first-section" style="background-image:url('images/slider-02.jpg');">
+
 					<div class="dtab">
+
 						<div class="container">
+
 							<div class="row">
+
 								<div class="col-md-12 col-sm-12 text-left">
+
 									<div class="big-tagline">
-										<h2 data-animation="animated zoomInRight">RestinPhilipDBSss <strong>education school</strong></h2>
+
+										<h2 class="text-center" data-animation="animated zoomInRight">Vincent <strong> High School</strong></h2>
+
 									</div>
+
 								</div>
+
 							</div><!-- end row -->            
+
 						</div><!-- end container -->
+
 					</div>
+
 				</div><!-- end section -->
+
 			</div>
+
 			<div class="carousel-item">
+
 				<div id="home" class="first-section" style="background-image:url('images/slider-03.jpg');">
+
 					<div class="dtab">
+
 						<div class="container">
+
 							<div class="row">
+
 								<div class="col-md-12 col-sm-12 text-center">
+
 									<div class="big-tagline">
-										<h2 data-animation="animated zoomInRight"><strong>Best</strong>education</h2>
+
+										<h2 data-animation="animated zoomInRight"><strong>Best</strong> education</h2>
+
 									</div>
+
 								</div>
+
 							</div><!-- end row -->            
+
 						</div><!-- end container -->
+
 					</div>
+
 				</div><!-- end section -->
+
 			</div>
+
 			<!-- Left Control -->
+
 			<a class="new-effect carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+
 				<span class="fa fa-angle-left" aria-hidden="true"></span>
+
 				<span class="sr-only">Previous</span>
+
 			</a>
 
+
+
 			<!-- Right Control -->
+
 			<a class="new-effect carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+
 				<span class="fa fa-angle-right" aria-hidden="true"></span>
+
 				<span class="sr-only">Next</span>
+
 			</a>
+
 		</div>
+
 	</div>
+
 	
+
     <div id="overviews" class="section wb">
+
         <div class="container">
+
             <div class="section-title row text-center">
+
                 <div class="col-md-8 offset-md-2">
+
                     <h3>About</h3>
+
                     <p class="lead">Lorem Ipsum dolroin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem!</p>
+
                 </div>
+
             </div><!-- end title -->
+
         
+
             <div class="row align-items-center">
+
                 <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12">
+
                     <div class="message-box">
+
                         <h4>2018 BEST RestinPhilipDBS education school</h4>
+
                         <h2>Welcome to RestinPhilipDBS education school</h2>
+
                         <p>Quisque eget nisl id nulla sagittis auctor quis id. Aliquam quis vehicula enim, non aliquam risus. Sed a tellus quis mi rhoncus dignissim.</p>
+
+
 
                         <p> Integer rutrum ligula eu dignissim laoreet. Pellentesque venenatis nibh sed tellus faucibus bibendum. Sed fermentum est vitae rhoncus molestie. Cum sociis natoque penatibus et magnis montes, nascetur ridiculus mus. Sed vitae rutrum neque. </p>
 
+
+
                         <a href="#" class="hover-btn-new orange"><span>Learn More</span></a>
+
                     </div><!-- end messagebox -->
+
                 </div><!-- end col -->
+
 				
+
 				<div class="col-xl-6 col-lg-6 col-md-12 col-sm-12">
+
                     <div class="post-media wow fadeIn">
+
                         <img src="images/about_02.jpg" alt="" class="img-fluid img-rounded">
+
                     </div><!-- end media -->
+
                 </div><!-- end col -->
+
 			</div>
+
 			<div class="row align-items-center">
+
 				<div class="col-xl-6 col-lg-6 col-md-12 col-sm-12">
+
                     <div class="post-media wow fadeIn">
+
                         <img src="images/about_03.jpg" alt="" class="img-fluid img-rounded">
+
                     </div><!-- end media -->
+
                 </div><!-- end col -->
+
 				
+
 				<div class="col-xl-6 col-lg-6 col-md-12 col-sm-12">
+
                     <div class="message-box">
+
                         <h2>The standard Lorem Ipsum passage, used since the 1500s</h2>
+
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+
 
                         <p> Integer rutrum ligula eu dignissim laoreet. Pellentesque venenatis nibh sed tellus faucibus bibendum.</p>
 
+
+
                         <a href="#" class="hover-btn-new orange"><span>Learn More</span></a>
+
                     </div><!-- end messagebox -->
+
                 </div><!-- end col -->
+
 				
+
             </div><!-- end row -->
+
         </div><!-- end container -->
+
     </div><!-- end section -->
+
+
 
 	<div class="section cl">
+
 		<div class="container">
+
 			<div class="row text-left stat-wrap">
+
 				<div class="col-md-4 col-sm-4 col-xs-12">
+
 					<span data-scroll class="global-radius icon_wrap effect-1 alignleft"><i class="flaticon-study"></i></span>
+
 					<p class="stat_count">12000</p>
+
 					<h3>Students</h3>
+
 				</div><!-- end col -->
 
+
+
 				<div class="col-md-4 col-sm-4 col-xs-12">
+
 					<span data-scroll class="global-radius icon_wrap effect-1 alignleft"><i class="flaticon-online"></i></span>
+
 					<p class="stat_count">240</p>
+
 					<h3>Courses</h3>
+
 				</div><!-- end col -->
 
+
+
 				<div class="col-md-4 col-sm-4 col-xs-12">
+
 					<span data-scroll class="global-radius icon_wrap effect-1 alignleft"><i class="flaticon-years"></i></span>
+
 					<p class="stat_count">55</p>
+
 					<h3>Years Completed</h3>
+
 				</div><!-- end col -->
+
 			</div><!-- end row -->
+
 		</div><!-- end container -->
+
 	</div><!-- end section -->
 
+
+
     <div id="testimonials" class="parallax section db parallax-off" style="background-image:url('images/parallax_04.jpg');">
+
         <div class="container">
+
             <div class="section-title text-center">
+
                 <h3>Testimonials</h3>
+
                 <p>Lorem ipsum dolor sit aet, consectetur adipisicing lit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+
             </div><!-- end title -->
 
+
+
             <div class="row">
+
                 <div class="col-md-12 col-sm-12">
+
                     <div class="testi-carousel owl-carousel owl-theme">
+
                         <div class="testimonial clearfix">
+
 							<div class="testi-meta">
+
                                 <img src="images/testi_01.png" alt="" class="img-fluid">
+
                                 <h4>James Fernando </h4>
+
                             </div>
+
                             <div class="desc">
+
                                 <h3><i class="fa fa-quote-left"></i> Wonderful Support!</h3>
+
                                 <p class="lead">They have got my project on time with the competition with a sed highly skilled, and experienced & professional team.</p>
+
                             </div>
+
                             <!-- end testi-meta -->
+
                         </div>
+
                         <!-- end testimonial -->
 
+
+
                         <div class="testimonial clearfix">
+
 							<div class="testi-meta">
+
                                 <img src="images/testi_02.png" alt="" class="img-fluid">
+
                                 <h4>Jacques Philips </h4>
+
                             </div>
+
                             <div class="desc">
+
                                 <h3><i class="fa fa-quote-left"></i> Awesome Services!</h3>
+
                                 <p class="lead">Explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you completed.</p>
+
                             </div>
+
                             <!-- end testi-meta -->
+
                         </div>
+
                         <!-- end testimonial -->
 
+
+
                         <div class="testimonial clearfix">
+
 							<div class="testi-meta">
+
                                 <img src="images/testi_03.png" alt="" class="img-fluid ">
+
                                 <h4>Venanda Mercy </h4>
+
                             </div>
+
                             <div class="desc">
+
                                 <h3><i class="fa fa-quote-left"></i> Great & Talented Team!</h3>
+
                                 <p class="lead">The master-builder of human happines no one rejects, dislikes avoids pleasure itself, because it is very pursue pleasure. </p>
+
                             </div>
+
                             <!-- end testi-meta -->
+
                         </div>
+
                         <!-- end testimonial -->
+
                         <div class="testimonial clearfix">
+
 							<div class="testi-meta">
+
                                 <img src="images/testi_01.png" alt="" class="img-fluid">
+
                                 <h4>James Fernando </h4>
+
                             </div>
+
                             <div class="desc">
+
                                 <h3><i class="fa fa-quote-left"></i> Wonderful Support!</h3>
+
                                 <p class="lead">They have got my project on time with the competition with a sed highly skilled, and experienced & professional team.</p>
+
                             </div>
+
                             <!-- end testi-meta -->
+
                         </div>
+
                         <!-- end testimonial -->
 
+
+
                         <div class="testimonial clearfix">
+
 							<div class="testi-meta">
+
                                 <img src="images/testi_02.png" alt="" class="img-fluid">
+
                                 <h4>Jacques Philips </h4>
+
                             </div>
+
                             <div class="desc">
+
                                 <h3><i class="fa fa-quote-left"></i> Awesome Services!</h3>
+
                                 <p class="lead">Explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you completed.</p>
+
                             </div>
+
                             <!-- end testi-meta -->
+
                         </div>
+
                         <!-- end testimonial -->
 
+
+
                         <div class="testimonial clearfix">
+
 							<div class="testi-meta">
+
                                 <img src="images/testi_03.png" alt="" class="img-fluid">
+
                                 <h4>Venanda Mercy </h4>
+
                             </div>
+
                             <div class="desc">
+
                                 <h3><i class="fa fa-quote-left"></i> Great & Talented Team!</h3>
+
                                 <p class="lead">The master-builder of human happines no one rejects, dislikes avoids pleasure itself, because it is very pursue pleasure. </p>
+
                             </div>
+
                             <!-- end testi-meta -->
+
                         </div><!-- end testimonial -->
+
                     </div><!-- end carousel -->
+
                 </div><!-- end col -->
+
             </div><!-- end row -->
+
         </div><!-- end container -->
+
     </div><!-- end section -->
+
+
 
     <div class="parallax section dbcolor">
+
         <div class="container">
+
             <div class="row logos">
+
                 <div class="col-md-2 col-sm-2 col-xs-6 wow fadeInUp">
+
                     <a href="#"><img src="images/logo_01.png" alt="" class="img-repsonsive"></a>
+
                 </div>
+
                 <div class="col-md-2 col-sm-2 col-xs-6 wow fadeInUp">
+
                     <a href="#"><img src="images/logo_02.png" alt="" class="img-repsonsive"></a>
+
                 </div>
+
                 <div class="col-md-2 col-sm-2 col-xs-6 wow fadeInUp">
+
                     <a href="#"><img src="images/logo_03.png" alt="" class="img-repsonsive"></a>
+
                 </div>
+
                 <div class="col-md-2 col-sm-2 col-xs-6 wow fadeInUp">
+
                     <a href="#"><img src="images/logo_04.png" alt="" class="img-repsonsive"></a>
+
                 </div>
+
                 <div class="col-md-2 col-sm-2 col-xs-6 wow fadeInUp">
+
                     <a href="#"><img src="images/logo_05.png" alt="" class="img-repsonsive"></a>
+
                 </div>
+
                 <div class="col-md-2 col-sm-2 col-xs-6 wow fadeInUp">
+
                     <a href="#"><img src="images/logo_06.png" alt="" class="img-repsonsive"></a>
+
                 </div>
+
             </div><!-- end row -->
+
         </div><!-- end container -->
+
     </div><!-- end section -->
+
 <?php getFooter(); ?>
